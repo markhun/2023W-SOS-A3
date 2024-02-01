7eb90740fd345782abb7b4aed51a2a57f8f93e35
Markus Hunner <26381538+markhun@users.noreply.github.com>
2024-02-01 21:45:09 +0100

    Add function to select top labels similar to LabelSOM.java

--- labelsom/src/labelsom/_labelsom.py
+++ labelsom/src/labelsom/_labelsom.py
@@ -32,3 +32,32 @@ def generate_unit_idx_to_mapped_indices_mapping(n, m, weights, input_data):
 
     return unit_i_to_mapped_inputs, hit_histogram
 
+
+def _select_top_labels(labels, number_of_labels_to_generate, dim):
+    """Method to select `number_of_labels_to_generate` top labels.
+
+    Direct 1:1 re-implementation from ``LabelSOM.Java``.
+    """
+    labels_sorted_by_mean = sorted(labels, key=lambda label: label.mean)
+    labels_sorted_by_qe = sorted(labels, key=lambda label: label.quantization_error)
+
+    # determine select num top labels
+    top_labels = [None for _ in range(number_of_labels_to_generate)]
+    found = 0
+    lab = 0
+
+    while (
+        found < number_of_labels_to_generate and lab < dim
+    ):  # go through list sorted by qe
+        found2 = False
+        lab2 = dim - 1
+        while not found2 and lab2 >= dim - number_of_labels_to_generate:
+            if labels_sorted_by_mean[lab2] == labels_sorted_by_qe[lab]:
+                found2 = True
+                top_labels[found] = labels_sorted_by_qe[lab]
+                found += 1
+            lab2 -= 1
+        lab += 1
+
+    return top_labels
+
