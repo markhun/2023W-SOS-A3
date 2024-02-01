a41b541e4c3fa6e041bc4cc1e0c8963ae5d163ec
Markus Hunner <26381538+markhun@users.noreply.github.com>
2024-02-01 21:41:39 +0100

    Add Label class similar to LabelSOM.java

--- labelsom/src/labelsom/_labelsom.py
+++ labelsom/src/labelsom/_labelsom.py
@@ -1,2 +1,10 @@
 import logging
+from dataclasses import dataclass
 LOGGER = logging.getLogger(__name__)
+
+@dataclass
+class Label:
+    label: str
+    mean: float
+    quantization_error: float
+
