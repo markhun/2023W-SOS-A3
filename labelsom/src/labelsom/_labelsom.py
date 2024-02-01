a48c9b7da79c5b6ca0c902a98acb30090c5f8001
Markus Hunner <26381538+markhun@users.noreply.github.com>
2024-02-01 21:55:07 +0100

    Add function to generate labels for the input data of a given unit

--- labelsom/src/labelsom/_labelsom.py
+++ labelsom/src/labelsom/_labelsom.py
@@ -1,8 +1,12 @@
 import logging
 from collections import defaultdict
 from dataclasses import dataclass
+
 import numpy as np
+import numpy.typing as npt
+
 LOGGER = logging.getLogger(__name__)
+
 
 @dataclass
 class Label:
@@ -11,7 +15,7 @@ class Label:
     quantization_error: float
 
 
-def generate_unit_idx_to_mapped_indices_mapping(n, m, weights, input_data):
+def generate_unit_idx_to_mapped_indices_mapping(m, n, weights, input_data):
     """Find mapped units similar to `PySOMVis/coding_assigment:HitHist` implementation
 
     Using mapping function provided by https://www.ifs.tuwien.ac.at/~andi/somlib/labelsom.html
@@ -61,3 +65,74 @@ def _select_top_labels(labels, number_of_labels_to_generate, dim):
 
     return top_labels
 
+
+def generate_label_for_unit(
+    vec_dim: int,
+    mapped_inputs: npt.ArrayLike,
+    weight_vector: npt.ArrayLike,
+    attribute_names: list[str],
+    number_of_labels_to_generate: int,
+    ignore_labels_with_zero: bool,
+) -> list[Label]:
+    """Generate the label data for the inputs of a given unit
+
+    Parameters
+    ----------
+    vec_dim
+        Dimension of input vectors.
+    mapped_inputs
+        Input vectors mapped to the unit for which to generate labels.
+    weight_vector
+        Weights for the given unit.
+    attribute_names
+        Attribute names to use for labeling.
+    number_of_labels_to_generate
+        Number of labels to generate. Can be at maximum `vec_dim`.
+    ignore_labels_with_zero
+        If labels with a mean of 0 should be ignored.
+
+    Returns
+    -------
+        A list of labels generated depending of the passed input data.
+
+    Raises
+    ------
+    RuntimeError
+        If `vec_dim` is not equal to passed number of attribute names.
+    """
+    if not vec_dim == len(attribute_names):
+        raise RuntimeError("Data dimension must match number of attributes !")
+
+    if len(mapped_inputs) == 0:
+        return []
+    if number_of_labels_to_generate > vec_dim:
+        number_of_labels_to_generate = vec_dim
+
+    means = np.mean(mapped_inputs, axis=0)
+    quantization_errors = np.mean(np.absolute(mapped_inputs - weight_vector), axis=0)
+
+    labels = []
+    for attribute_idx in range(vec_dim):
+        mean = means[attribute_idx]
+        quantization_error = quantization_errors[attribute_idx]
+
+        # ignoreLabelsWithZero criteria from ``LabelSOM.java``:
+        #
+        # if we shall ignore zero labels, ignore those with mean==0, and very small qe
+        if ignore_labels_with_zero and mean == 0 and quantization_error * 100 < 0.1:
+            labels.append(Label("", mean, quantization_error))
+        else:
+            labels.append(
+                Label(attribute_names[attribute_idx], mean, quantization_error)
+            )
+
+    labels = _select_top_labels(labels, number_of_labels_to_generate, vec_dim)
+
+    # LabelSOM.java executes:
+    #
+    #       Label.sortByValueQe(labels, Label.SORT_DESC, Label.SORT_ASC);
+    #
+    # which is kinda hard to mimic in Python
+    labels = sorted(labels, key=lambda label: (-label.mean, label.quantization_error))
+
+    return labels
